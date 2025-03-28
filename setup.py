b105f0f706e968481febc8540794bc0d7881ede7
Ubuntu <ubuntu@ip-172-31-28-78.eu-north-1.compute.internal>
2025-03-28 15:34:01 +0000

    auto-install requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+    
 setup(
     name="calle",
-    version="1.0",
+    version="1.0.0",
     packages=find_packages(),
+    install_requires=requirements,
 )
