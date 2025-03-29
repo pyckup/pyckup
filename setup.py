0c0667f94b95ead0717435c2842dd1cbcd6217aa
Max <102767862+ruetzmax@users.noreply.github.com>
2025-03-29 13:39:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
     
 setup(
     name="calle",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
