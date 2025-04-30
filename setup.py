36d9cf6987752010712768f69556cdb6a4bd7cde
Max <102767862+ruetzmax@users.noreply.github.com>
2025-04-30 07:02:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="pyckup",
-    version="1.0.2",
+    name="pyckup-lib",
+    version="1.0.3",
     packages=find_packages(),
     install_requires=requirements,
     package_data={"pyckup": ["resources/*", "conf/*"]},
